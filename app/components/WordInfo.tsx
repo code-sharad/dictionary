3a52224a5a88a3b1e3f006a5483a6d7449dd1bf9
Sharad Bhadait <61672294+code-sharad@users.noreply.github.com>
2025-08-15 17:13:21 +0530

    Update WordInfo.tsx

--- app/components/WordInfo.tsx
+++ app/components/WordInfo.tsx
@@ -124,9 +124,6 @@ function PlayIcon() {
 function PauseIcon() {
     return <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="6" y="4" width="4" height="16"/><rect x="14" y="4" width="4" height="16"/></svg>;
 }
-}
-
-export default WordInfo;
 
 function PlayIcon() {
     return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-play"><polygon points="6 3 20 12 6 21 6 3" /></svg>
